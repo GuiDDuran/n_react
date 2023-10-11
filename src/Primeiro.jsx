255dc73e0e7819fd737c4e903697e9c785aa25cd
Guilherme Duran Duran Gea <guilherme.d.gea@gmail.com>
2023-10-11 10:57:08 -0300

    Atualização da página

--- src/Primeiro.jsx
+++
@@ -1,8 +0,0 @@
-const Primeiro = () => {
-    return (<div>
-                <h5>Mini Título</h5>
-                <p>Meu texto</p> 
-            </div>);
-};
-
-export default Primeiro;
---
+++ src/pages/Primeiro.jsx
@@ -0,0 +1,10 @@
+import LinkSegundo from "../components/LinkSegundo";
+
+const Primeiro = () => {
+    return (<div>
+                <h1>Primeiro</h1>
+                <LinkSegundo /> 
+            </div>);
+};
+
+export default Primeiro;
